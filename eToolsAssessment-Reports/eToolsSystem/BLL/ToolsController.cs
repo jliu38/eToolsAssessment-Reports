0c8a9d100361283d4cdbb981c1dd7e9ad0c09ad5
jliu38 <jliu38@studentmail.nait.ca>
2014-11-18 13:07:33 -0700

    eToolsAssessment-Reports

---
+++ eToolsAssessment-Reports/eToolsSystem/BLL/ToolsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel;
+using eToolsSystem.Entities;
+using eToolsSystem.Entities.POCOs;
+using eToolsSystem.DAL;
+#endregion
+
+namespace eToolsSystem.BLL
+{
+    [DataObject]
+    public class ToolsController
+    {
+        #region Position List
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Position> Positions_List()
+        {
+            using (ToolsContext context = new ToolsContext())
+            {
+                return context.Positions.ToList();
+            }
+        }
+
+
+        #endregion
+
+        #region Assessment Report Linq
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ReportEmployee>Employee_byPositionReport(int positionid)
+        {
+            if (positionid == 0)
+            {
+                using (var context = new ToolsContext())
+                {
+                    var results = from cat in context.Employees
+                                  orderby cat.Positions.Description
+                                  select new ReportEmployee()
+                                  {
+                                      Position = cat.Positions.Description,
+                                      Name = cat.LastName + " " + cat.FirstName,
+                                      Hired = cat.DateHired,
+                                      Release = cat.DateReleased,
+                                  };
+
+                    return results.ToList();
+                }
+            }
+            else
+            {
+                using (var context = new ToolsContext())
+                {
+                    var results = from cat in context.Employees
+                                  orderby cat.Positions.Description
+                                  where cat.Positions.PositionID == positionid
+                                  select new ReportEmployee()
+                                  {
+                                      Position = cat.Positions.Description,
+                                      Name = cat.LastName + " " + cat.FirstName,
+                                      Hired = cat.DateHired,
+                                      Release = cat.DateReleased,
+                                  };
+
+                    return results.ToList();
+                }
+            }
+            
+        }
+        #endregion
+
+
+    }
+}
---
+++ eToolsAssessment-Reports/eToolsWeb/Assessments/EmployeeListReport.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="EmployeeListReport.aspx.cs" Inherits="Assessments_EmployeeListReport" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<%@ Register Src="~/UserControls/MessageUserControl.ascx" TagPrefix="my" TagName="MessageUserControl" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    
+    <center><h1>Assessment: Report</h1></center>
+    Select the position:&nbsp;&nbsp;
+    <asp:DropDownList ID="PositionList" runat="server" DataSourceID="ODSPositions" DataTextField="Description" DataValueField="PositionID" AppendDataBoundItems="True" AutoPostBack="True">
+        <asp:ListItem Value="-1">Position ??</asp:ListItem>
+        <asp:ListItem Value="0">All</asp:ListItem>
+    </asp:DropDownList>
+    &nbsp;&nbsp;Press the report refresh button.
+    
+    
+
+
+    <my:MessageUserControl runat="server" ID="MessageUserControl" />
+    <asp:ObjectDataSource ID="ObjectDataSource1" runat="server"
+         OldValuesParameterFormatString="original_{0}" 
+        SelectMethod="Employee_byPositionReport" TypeName="eToolsSystem.BLL.ToolsController">
+        <SelectParameters>
+            <asp:ControlParameter ControlID="PositionList"
+                 PropertyName="SelectedValue" Name="positionid" Type="Int32"></asp:ControlParameter>
+        </SelectParameters>
+    </asp:ObjectDataSource>
+
+    <rsweb:ReportViewer ID="ReportViewer1" runat="server" Font-Names="Verdana" Font-Size="8pt" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" Width="100%" >
+        <LocalReport ReportPath="Assessments\EmployeeListReport.rdlc">
+            <DataSources>
+                <rsweb:ReportDataSource Name="DataSet1" DataSourceId="ObjectDataSource1"></rsweb:ReportDataSource>
+            </DataSources>
+        </LocalReport>
+    </rsweb:ReportViewer>
+
+     <asp:ObjectDataSource ID="ODSPositions" runat="server" 
+        OldValuesParameterFormatString="original_{0}" 
+        SelectMethod="Positions_List" 
+        TypeName="eToolsSystem.BLL.ToolsController" >
+    </asp:ObjectDataSource>
+
+    </asp:Content>
+
